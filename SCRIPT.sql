35f55c859cd6370aac24e2bf9de572c59383de36
Cubiasss-Eduardo <20220229@ricaldone.edu.sv>
2024-02-28 10:38:13 -0600

    procedimientos almacenados listos

--- SCRIPT.sql
+++ SCRIPT.sql
@@ -38,6 +38,7 @@ id_genero_libro BINARY(36),
 estado ENUM('activo', 'inactivo', 'pendiente')
 );
 
+-- Constraints con nombres, aqui van las foreign keys
 ALTER TABLE prestamos
 ADD CONSTRAINT fk_cliente_prestamo FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente);
 
@@ -50,6 +51,7 @@ ADD CONSTRAINT fk_libros_detalles_prestamos FOREIGN KEY (id_libro) REFERENCES li
 ALTER TABLE libros
 ADD CONSTRAINT fk_generos_libros_libros FOREIGN KEY (id_genero_libro) REFERENCES generos_libros(id_genero_libro);
 
+-- Constraints con nombres, aqui van los check y deafault, porque solo esos encontramos necesarios
 ALTER TABLE clientes
 ADD CONSTRAINT ch_telefono_clientes CHECK(LENGTH(telefono) > 8);
 
@@ -61,3 +63,76 @@ ALTER COLUMN fecha_devolucion SET DEFAULT '2024-01-02';
 
 ALTER TABLE libros 
 ADD CONSTRAINT ch_anio_publicacion_libros CHECK(anio_publicacion > 1990);
+
+-- Procedimientos almacenados para cada tabla
+
+-- Tabla clientes
+
+DELIMiTER //
+
+CREATE PROCEDURE agregar_cliente(nombre VARCHAR(50), email VARCHAR(100), telefono VARCHAR(10))
+BEGIN
+        
+    INSERT INTO clientes(nombre_cliente, email_cliente, telefono) VALUES (nombre, email, telefono);
+
+END //
+
+DELIMITER ;
+
+
+-- Tabla generos_libros
+
+DELIMiTER //
+
+CREATE PROCEDURE agregar_generos_libros(nombre VARCHAR(50))
+BEGIN
+        
+    INSERT INTO generos_libros(nombre_genero_libro) VALUES (nombre);
+
+END //
+
+DELIMITER ;
+
+
+-- Tabla prestamos
+
+DELIMiTER //
+
+CREATE PROCEDURE agregar_prestamos(id_clientee BINARY(36), fecha_i DATE, fecha_d DATE, estado ENUM('activo', 'inactivo', 'pendiente'))
+BEGIN
+        
+    INSERT INTO prestamos(id_cliente, fecha_inicio, fecha_devolucion, estado) VALUES (id_clientee, fecha_i, fecha_d, estado);
+
+END //
+
+DELIMITER ;
+
+
+-- Tabla detalle_prestamos
+
+DELIMiTER //
+
+CREATE PROCEDURE agregar_detalles_prestamos(id_prestamoo BINARY(36), id_libroo BINARY(36))
+BEGIN
+        
+    INSERT INTO detalles_prestamos(id_prestamo, id_libro) VALUES (id_prestamoo, id_libroo);
+
+END //
+
+DELIMITER ;
+
+
+-- Tabla libros
+
+DELIMiTER //
+
+CREATE PROCEDURE agregar_libros(titulo_libroo VARCHAR(50), anio_publicacionn INT, id_genero_libroo BINARY(36), estadoo ENUM('activo', 'inactivo', 'pendiente'))
+BEGIN
+        
+    INSERT INTO libros(titulo_libro, anio_publiacion, id_genero_libro, estado) VALUES (titulo_libroo, anio_publicacionn, id_genero_libroo, estado);
+
+END //
+
+DELIMITER ;
+
+
